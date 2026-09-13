40af503e46b4c9af33be23d543ae6a98d2d4fbe6
Jemi <mjemi4025@gmail.com>
2026-09-13 19:12:47 +0530

    Fix indentation

--- siteforge/app.py
+++ siteforge/app.py
@@ -309,5 +309,5 @@ def edit(site_id):
 
    init_db()
 
-   if __name__ == "__main__":
-       app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
